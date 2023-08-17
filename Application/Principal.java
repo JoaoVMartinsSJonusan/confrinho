5eb4bec487313186402ab9ab59766750189eff60
Joao Vitor Martins <joaovitormartins338@gmail.com>
2023-08-16 21:53:34 -0300

    Inclusao de menu e conrreção de bug

--- Application/Principal.java
+++ Application/Principal.java
@@ -22,71 +22,73 @@ public class Principal {
         Scanner sc = new Scanner(System.in);
         limpartela();
         Cofrinho cofre= new Cofrinho();
-        char escolha;
+        int escolhaMenu;
         //um try-catch para caso o usuario escreva algo que nao seja um numero e evite assim o erro do programa
         
         try {
             do {
                 cofre.listagemMoedas();
-                System.out.println("------------------------------------------------------------");
-                System.out.print("Deseja adicionar [1] DOLAR, [2]EURO ou [3]REAL: ");
-                int escolhaMoeda = sc.nextInt();
-                Double valor = 0.0;
-                
-                //De acordo com a escolha do usuario é feita um polimorfismo adicionando os valores digitados a lista na classe Cofrinho
-                switch (escolhaMoeda) {
+                System.out.println("================");
+                System.out.println("----------");
+                System.out.println("---MENU---");
+                System.out.println("----------");
+                System.out
+                        .println("Escolha sua opção:\n1) Despositar \n2) retirar  \n3) Converter para real \n4) Sair");
+                System.out.print("Escolha sua opção: ");
+                escolhaMenu = sc.nextInt();
+                switch (escolhaMenu) {
                     case 1:
-                        System.out.print("digite o valor a ser depositado: ");
-                        valor = sc.nextDouble();
-                        cofre.adicionar(new Dolar(valor));
+                        System.out.print("Deseja adicionar [1] DOLAR, [2]EURO ou [3]REAL: ");
+                        int escolhaMoeda = sc.nextInt();
+                        Double valor = 0.0;
+
+                        //De acordo com a escolha do usuario é feita um polimorfismo adicionando os valores digitados a lista na classe Cofrinho
+                        switch (escolhaMoeda) {
+                            case 1:
+                                System.out.print("digite o valor a ser depositado: ");
+                                valor = sc.nextDouble();
+                                cofre.adicionar(new Dolar(valor));
+                                limpartela();
+                                break;
+                            case 2:
+                                System.out.print("digite o valor a ser depositado: ");
+                                valor = sc.nextDouble();
+                                cofre.adicionar(new Euro(valor));
+                                limpartela();
+                                break;
+                            case 3:
+                                System.out.print("digite o valor a ser depositado: ");
+                                valor = sc.nextDouble();
+                                cofre.adicionar(new Real(valor));
+                                limpartela();
+                                break;
+                            default:
+                                break;
+                        }
                         break;
                     case 2:
-                        System.out.print("digite o valor a ser depositado: ");
-                        valor = sc.nextDouble();
-                        cofre.adicionar(new Euro(valor));
+                        limpartela();
+                        System.out.println();
+                        cofre.listagemMoedas();
+                        System.out.println();
+                        System.out.print("Digite o valor a ser retirado: ");
+                        double valorRemover = sc.nextDouble();
+                        cofre.remover(valorRemover);
+                        limpartela();
                         break;
                     case 3:
-                        System.out.print("digite o valor a ser depositado: ");
-                        valor = sc.nextDouble();
-                        cofre.adicionar(new Real(valor));
+                        limpartela();
+                        cofre.totalConvertido();
                         break;
                     default:
                         break;
                 }
-                System.out.print("Deseja continuar adicionando valores? [S]/[N] ");
-                escolha = sc.next().toUpperCase().charAt(0);
-                limpartela();
-            } while (escolha != 'N');
+            } while (escolhaMenu != 4);
 
-            cofre.listagemMoedas();
-            //Linhas de comando para remoção de valores da lista
-            System.out.print("Deseja remover algum valor? [S]/[N] ");
-            escolha = sc.next().toUpperCase().charAt(0);
-            while(escolha == 'S'){
-                System.out.println();
-                cofre.listagemMoedas();
-                System.out.println();
-                System.out.print("Digite o valor a ser retirado: ");
-                double valorRemover = sc.nextDouble();
-                cofre.remover(valorRemover);
-                limpartela();
-                cofre.listagemMoedas();
-                System.out.print("Deseja remover outro valor? [S]/[N] ");
-                escolha = sc.next().toUpperCase().charAt(0);
-                limpartela();
-            }
-                
-        } catch(InputMismatchException e){
+        } 
+        catch(InputMismatchException e){
             System.out.println("Erro InputMismatchException: Digitou algo incorretamente.");
         }
-
-        System.out.println();
-
-        cofre.listagemMoedas();
-        System.out.println();
-        cofre.totalConvertido();
-
-        
         sc.close();
     }
 }
--- Cofre/Cofrinho.java
+++ Cofre/Cofrinho.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 
 public class Cofrinho {
-    //Lista onde as moedas serao adicionadas
+    //Lista onde as moedas serao adicionadas.
     private List<Moeda> moedas;
 
     private double convertido;
@@ -22,12 +22,12 @@ public class Cofrinho {
 
 
     public void remover(Double moeda) {
-        //Laço for para percorrer todo a lista verificando se tem o valor que quer ser removido
+        //Laço for para percorrer todo a lista verificando se tem o valor que quer ser removido.
         for (int i = 0; i < moedas.size(); i++) {
-            //variavel Booleana para saber se foi ou nao encontrado o valor para remover
+            //variavel Booleana para saber se foi ou nao encontrado o valor para remover.
             boolean temOuNao = false;
             Moeda m = moedas.get(i);
-            //Comando if para saber se o valor na posição "i" da lista é igual o valor solicitado para remoção
+            //Comando if para saber se o valor na posição "i" da lista é igual o valor solicitado para remoção.
             if (m.getValor().equals(moeda)) {
                 temOuNao = true;
                 moedas.remove(m);
@@ -49,15 +49,21 @@ public class Cofrinho {
 
 
     public void totalConvertido() {
+        int controle = 0;
         for (Moeda moeda : moedas) {
-            //Verificação feita para saber o valor é da classe real ou nao, ja que quando tinha somente o moeda.converter(), chamava o metodo do real perguntando para qual moeda queria converter, porem essa função totalConvertido() serve para retornar o valor total convertido em reais
+            //Verificação feita para saber o valor é da classe real ou nao, ja que quando tinha somente o moeda.converter(), chamava o metodo do real perguntando para qual moeda queria converter, porem essa função totalConvertido() serve para retornar o valor total convertido em reais.
             if (moeda instanceof Real) {
                 convertido += moeda.getValor();
             } else {
                 convertido += moeda.converter();
+                controle++;
             }
         }
-        System.out.println("Saldo total convertido para Reais: R$" + String.format("%.2f",convertido));
+        System.out.println("Saldo total convertido para Reais: R$" + String.format("%.2f", convertido));
+        //Serve para saber se ja foi convertido, pois quando chamava sem esse controle varias vezes seguidas o valor se duplicava, essa linha de codigo abaixo faz com que isso seja evitado, fazendo com que o valor convertido seja exatamente sobre o valor que esta na carteira naquele exato momento.
+        if(controle > 0){
+            convertido = 0;
+        }
     }
 
     
